594aa81b440eb712c0471328fd61d6412390885a
jsw <954951185@qq.com>
2024-06-01 18:00:16 +0800

    万能头

---
+++ any-head.h
@@ -0,0 +1,18 @@
+//
+// Created by jsw on 24-6-1.
+//
+
+#ifndef ANY_HEAD_H
+#define ANY_HEAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <stdint.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+
+#endif //ANY_HEAD_H
